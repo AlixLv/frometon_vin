ccfff58a3d18892e0fdc155a8cbe3620fa2c34b9
Alix Levé <alix.leve@gmail.com>
2025-01-10 10:47:26 +0100

    add: context_processors.py and navbar in base.html

---
+++ frometon/context_processors.py
@@ -0,0 +1,16 @@
+from products.models import Cheese, Wine
+
+def cheese_categories(request):
+    # values_list pour retourner une liste de valeurs sur le champs "type_of_milk"
+    # argument flat=True pour indiquer qu'on veut une liste simple de valeurs, pas de tuples
+    # distinct() pour garantir des valeurs uniques
+    types_of_milk = Cheese.objects.values_list('type_of_milk', flat=True).distinct()
+    print("🌸", types_of_milk)
+    return {'cheese_categories': types_of_milk}
+
+
+def wine_categories(request):
+    colors = Wine.objects.values_list('color', flat=True).distinct()
+    print("🌼", colors)
+    return {'wine_categories': colors}
+
