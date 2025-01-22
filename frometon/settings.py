bbface5f4cbf5bb18d1283a5d91360dc9189af52
Alix Levé <alix.leve@gmail.com>
2025-01-22 15:14:49 +0100

    update menu

--- frometon/settings.py
+++ frometon/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
+
+load_dotenv() # charge les configurations incluses dans .env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = frometon
