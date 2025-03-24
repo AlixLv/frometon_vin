6083c00099baeb3fa679905d2a38e70dbf5d448c
Alix <alix.leve@gmail.com>
2025-03-24 15:19:22 +0100

    add tests for user

--- users/tests.py
+++ users/tests.py
@@ -1,22 +1,49 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 
 class UsersTests(TestCase):
-
+    
+    def setUp(self):
+        # créer un utilisateur de test
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username= "testuser", 
+            email="normal@user.com", 
+            password="foo"
+            )
+    
     def test_create_user(self):
         User = get_user_model()
-        user = User.objects.create_user(email="normal@user.com", password="foo")
-        self.assertEqual(user.email, "normal@user.com")
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email="")
+       
+        # vérifier que le user a été créé correctement
+        self.assertEqual(self.user.email, "normal@user.com")
+        self.assertEqual(self.user.username, "testuser")
+       
+       # tester la création avec des valeurs invalides
         with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo")
+            User.objects.create_user(username="", email="test@example.com", password="foo")
+        
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="test2", email="", password="foo")    
+
+    def test_authenticate_user_using_login(self):
+        # connexion du user
+        login_successful = self.client.login(username= "testuser", email="normal@user.com", password="foo")
+        self.assertTrue(login_successful)
+        
+        # vérifier si user connecté a bien accès à la homepage
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_user_get_profile(self):
+        login_successful = self.client.login(username= "testuser", email="normal@user.com", password="foo")
+        self.assertTrue(login_successful)
+        
+        response = self.client.get('/users/profile/{}'.format(self.user.id), follow=True, HTTP_ACCEPT ='application/json')
+        # print("🟣 ", response.status_code)
+        # print(response.context)
+        # print("🟡 ", response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['username'], 'testuser')
+        self.assertEqual(response.context['email'], 'normal@user.com')
